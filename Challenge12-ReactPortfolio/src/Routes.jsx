97a588531603097d9f569be1308a850859cbb82f
quinnw10 <jquinnw10@gmail.com>
2024-10-26 18:11:22 -0400

    jsx files

---
+++ Challenge12-ReactPortfolio/src/Routes.jsx
@@ -0,0 +1,26 @@
+// src/AppRouter.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<AboutMe />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default AppRouter;
